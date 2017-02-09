a761932ce594d409319c06a3bf96951adaad5a1b
Chuck Williams <chuckwilliams37@gmail.com>
2017-02-09 01:03:37 -0700

    WIP: send-contact-email.

--- src/js/contact.js
+++ src/js/contact.js
@@ -9,6 +9,17 @@
 
 			console.log(name, email, category, message);
 			$(this).closest('form').find('input[type=text], textarea').val('');
+
+			$.post("http://localhost:8080/send-contact-email/",{
+				from: name,
+				email: email,
+				subject: category,
+				message: message
+			}, function(response){
+				console.info('Message sent: ' , response);
+			})
+
+
 			$('#msg_sent').css('display', 'inline-block');
 			$('#msg_sent').fadeOut(3000);
 		});
